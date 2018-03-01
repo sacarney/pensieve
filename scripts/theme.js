beae20936c9e2aa7a7b714d2877cc0a8b7fc31f7
sacarney <sacarney@iastate.edu>
2018-02-28 19:52:11 -0600

    Add the mobile menu js to the theme.js

--- scripts/theme.js
+++ scripts/theme.js
@@ -94,3 +94,31 @@ var smf_addListItemHoverEvents = function()
 // Add hover events to list items if the browser requires it.
 if (is_ie7down && 'attachEvent' in window)
 	window.attachEvent('onload', smf_addListItemHoverEvents);
+
+
+// MENU
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
